52a3005d3f5f56f9ed0168620ed612ec71139bd2
lifestealer86 <lifestealer@yandex.ru>
2025-02-26 15:33:07 +0700

    about

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            "data" => ["title" => ["name" => "Сибирская природа"],
+                "text" => ["data" => "Озера глубиной в тысячи метров, горные хребты, вековая тайга и северное сияние"],
+                "img" => ["url" => "img/siberian.jpeg"]]
+        ])->header('Content-Type', 'application/json');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/api-travel/siberian-nature', [AboutController::class, 'about']);
+Route::post('/authorization', [UserController::class, 'authorization']);
+Route::post('/registration', [UserController::class, 'registration']);
+Route::get('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::prefix('/api-travel')->middleware('auth:sanctum')->group(function () {
+    Route::get('/events', [EventController::class, 'show']);
+    Route::get('/events/{{%id}}', [EventController::class, 'show']);
+    Route::post('/events', [EventController::class, 'store']);
+    Route::patch('/events/{{%id}}', [EventController::class, 'patchEvent']);
+    Route::delete('/events/{{%id}}', [EventController::class, 'delete']);
+    Route::delete('/events/{{%id}}/response', [EventController::class, 'storeResponse']);
+    Route::post('/book-event', [EventController::class, 'storeBookEvent']);
+    Route::get('/search', [EventController::class, 'search']);
+    Route::get('/users/{{%id}}', [UserController::class, 'show']);
+    Route::patch('/users/{{%id}}', [UserController::class, 'patchUser']);
+    Route::post('/users/{{%id}}', [UserController::class, 'addPeople']);
 });
