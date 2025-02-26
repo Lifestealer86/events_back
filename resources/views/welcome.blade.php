2aed5e78350b69d7713f59586450dd000e58a2fd
lifestealer86 <lifestealer@yandex.ru>
2025-02-26 13:49:53 +0700

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Приветствие!</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="shortcut icon" href="{{asset('favicon.png')}}" type="image/png">
+</head>
+<body>
+<h1>
+    <span>404</span>
+    <span>Тут ничего нет и не будет!</span>
+</h1>
+</body>
+</html>
